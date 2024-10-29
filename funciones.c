1a18290276557cd947bf296f8d54f8be6b5c4837
sofi16mar <157255753+sofi16mar@users.noreply.github.com>
2024-10-29 10:13:50 -0500

    Update funciones.c

--- funciones.c
+++ funciones.c
@@ -1,49 +1,72 @@
 #include <stdio.h>
 #include <string.h>
-#include "funciones.h"
-switch (abcd)
-{
-case 1:
-    void Ingresarnombre(char nombre[5][50], int edad[5], char estadocivil[5][50])
-    {for (nombres i = 0; i < count; i++)
-    {
-        printf("Ingresar nombres de Personas %c" , &nombres);
-        fgets[nombre, [50], stdin];
+
+#define COUNT 5
+
+void Ingresarnombre(char nombre[COUNT][50]) {
+    for (int i = 0; i < COUNT; i++) {
+        printf("Ingresar nombre de Persona %d: ", i + 1);
+        fgets(nombre[i], 50, stdin);
+        // Eliminar el salto de línea al final de fgets
+        nombre[i][strcspn(nombre[i], "\n")] = '\0';
     }
-    } 
-case 2:
-    void Ingresaredad(charnombre[50], int edad[5], char estadocivil[5][50])
-    {for (edad i = 0; i < count; i++)
-    {
-        printf("Ingresar edad de Personas %d" , &edad);
-        fgets[edad, [50], stdin];
-        if (edad<=0 && edad >100)
-        {
-            printf("Edades erroneas, ingrese nuevamente");
-            i--;
+}
+
+void Ingresaredad(int edad[COUNT]) {
+    for (int i = 0; i < COUNT; i++) {
+        printf("Ingresar edad de Persona %d: ", i + 1);
+        scanf("%d", &edad[i]);
+        while (edad[i] <= 0 || edad[i] > 100) {
+            printf("Edad errónea, ingrese nuevamente: ");
+            scanf("%d", &edad[i]);
         }
     }
-    } 
-case 3:
-    void Ingresarestadocivil(char nombre[5][50], int edad[5], char estadocivil[5][50])
-    {for (estadocivil i = 0; i < count; i++)
-    {
-        printf("Ingresar estado civil de Personas %c" , &estadocivil);
-        fgets[estadocivil, [50], stdin];
+    // Limpiar el buffer de entrada
+    while (getchar() != '\n');
+}
+
+void Ingresarestadocivil(char estadocivil[COUNT][50]) {
+    for (int i = 0; i < COUNT; i++) {
+        printf("Ingresar estado civil de Persona %d: ", i + 1);
+        fgets(estadocivil[i], 50, stdin);
+        // Eliminar el salto de línea al final de fgets
+        estadocivil[i][strcspn(estadocivil[i], "\n")] = '\0';
     }
-    } 
-case 4:
-    printf("Desea otra opcion")
-    switch (opcion)
-    {
-    case 1:
-        printf("Alguno de nuestros asesores se comunicará con usted, gracias por su visita");
-        break;
-    
-    default:
-        break;
+}
+
+int main() {
+    char nombre[COUNT][50];
+    int edad[COUNT];
+    char estadocivil[COUNT][50];
+    int abcd;
+
+    printf("Ingrese una opción (1-4): ");
+    scanf("%d", &abcd);
+    // Limpiar el buffer de entrada
+    while (getchar() != '\n');
+
+    switch (abcd) {
+        case 1:
+            Ingresarnombre(nombre);
+            break;
+        case 2:
+            Ingresaredad(edad);
+            break;
+        case 3:
+            Ingresarestadocivil(estadocivil);
+            break;
+        case 4:
+            printf("¿Desea otra opción? (1 para sí, 0 para no): ");
+            int opcion;
+            scanf("%d", &opcion);
+            if (opcion == 1) {
+                printf("Alguno de nuestros asesores se comunicará con usted, gracias por su visita.\n");
+            }
+            break;
+        default:
+            printf("Opción no válida.\n");
+            break;
     }
-    
-default:
-    break;
+
+    return 0;
 }
